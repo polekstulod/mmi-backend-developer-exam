539a4b66916f768ee272a44a6a5415eae20edd2f
James Paul Tulod <paultulod@pm.me>
2024-02-20 21:37:15 +0800

    Update database name

--- config/database.php
+++ config/database.php
@@ -38,7 +38,7 @@ return [
         'mongodb' => [
             'driver' => 'mongodb',
             'dsn' => env('MONGODB_URI'),
-            'database' => 'YOUR_DATABASE_NAME',
+            'database' => 'crud_db',
         ],
 
         'sqlite' => [
