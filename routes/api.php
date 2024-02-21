8a53ee8ba1a93e59f4399a6326aa77f887ee072a
James Paul Tulod <paultulod@pm.me>
2024-02-21 23:19:04 +0800

    Create routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,17 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Categories
+Route::get('/categories', 'App\Http\Controllers\CategoryController@index');
+Route::get('/categories/{id}', 'App\Http\Controllers\CategoryController@show');
+Route::post('/categories', 'App\Http\Controllers\CategoryController@store');
+Route::put('/categories/{id}', 'App\Http\Controllers\CategoryController@update');
+Route::delete('/categories/{id}', 'App\Http\Controllers\CategoryController@destroy');
+
+// Items
+Route::get('/items', 'App\Http\Controllers\ItemController@index');
+Route::get('/items/{id}', 'App\Http\Controllers\ItemController@show');
+Route::post('/items', 'App\Http\Controllers\ItemController@store');
+Route::put('/items/{id}', 'App\Http\Controllers\ItemController@update');
+Route::delete('/items/{id}', 'App\Http\Controllers\ItemController@destroy');
